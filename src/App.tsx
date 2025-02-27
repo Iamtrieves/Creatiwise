28112b0ae7d84e2b442ba5368467288e548cdf3b
Gabriel Indok David <indokdavid2020@gmail.com>
2025-02-27 09:38:34 +0100

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 // import { motion } from "motion/react";
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="text-2xl font-bold italic">
+      This is my first successful commit
+    </div>
+  );
 };
 
 export default App;

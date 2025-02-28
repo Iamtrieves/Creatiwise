e85dd49713b4b20ac6488eee3a85675ce3d6ee2f
Gabriel Indok David <indokdavid2020@gmail.com>
2025-02-28 22:22:13 +0100

    Updated

--- src/components/ProjectStatsSection.tsx
+++ src/components/ProjectStatsSection.tsx
@@ -10,7 +10,7 @@ const ProjectStatsSection = (): React.ReactElement => {
       <section className="lg:py-[1.4875rem] py-[1rem] text-white lg:text-[1.875rem] text-[1.275rem] flex justify-center items-center bg-gradient-to-r from-[#1E1E1E] to-[#6C7075]">
         Over 200+ Projects delivered
       </section>
-      <section className="lg:mt-[7rem] mt-[4rem] lg:mx-[6rem] mx-[2rem] flex flex-col lg:gap-[2.5rem] gap-[1.5rem]">
+      <section className="lg:pt-[7rem] pt-[4rem] lg:px-[4rem] px-[1.2rem] flex flex-col lg:gap-[2.5rem] gap-[1.5rem]">
         <section>
           <div className="font-bold lg:text-[3.125rem] md:text-[2.5rem] text-[2rem] text-[#1E1E1E] text-center">
             About Creatiwise
@@ -28,17 +28,19 @@ const ProjectStatsSection = (): React.ReactElement => {
           <div className="font-bold text-[#1E1E1E] lg:text-[3.125rem] text-[2rem]">
             How We Work
           </div>
-          <div className="text-center lg:text-[2rem] text-[1.1rem] text-white lg:p-2 p-[8px] rounded-2xl lg:w-[60%] w-[70%] bg-gradient-to-r from-[#1E1E1E] to-[#6C7075]">
+          <div className="text-center lg:text-[2rem] text-[1rem] text-white lg:p-2 p-[8px] rounded-2xl lg:w-[60%] w-[70%] bg-gradient-to-r from-[#1E1E1E] to-[#6C7075]">
             Design Driven Strategy Led
           </div>
         </section>
         <section className="flex lg:mt-[4rem] md:mt-[4rem] mt-[2rem] flex-col gap-[4rem]">
           <div className="flex lg:flex-row md:flex-row flex-col w-full gap-[2rem] items-center justify-center">
-            <div className="lg:w-[20%] w-[10rem]">
+            <div className="lg:w-[20%] w-[8rem]">
               <img className="size-full" src={DefineIcon} alt="Define Icon" />
             </div>
-            <div className="lg:w-[80%] w-full">
-              <div className="font-bold text-[2.25rem]">1. Define</div>
+            <div className="lg:w-[80%] flex flex-col lg:gap-[2.9375rem] gap-[0.7rem] w-full">
+              <div className="font-bold lg:text-[2.25rem] text-[1rem] lg:text-start text-center">
+                1. Define
+              </div>
               <div className="lg:text-[1.45rem] text-[1rem] text-justify">
                 We prioritize your audience's needs, defining and converting
                 their ideas into reality. We believe that sharing more
@@ -48,11 +50,13 @@ const ProjectStatsSection = (): React.ReactElement => {
             </div>
           </div>
           <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col w-full gap-[2rem] items-center justify-center">
-            <div className="lg:w-[20%] w-[10rem]">
+            <div className="lg:w-[20%] w-[8rem]">
               <img className="size-full" src={DesignIcon} alt="Design Icon" />
             </div>
-            <div className="lg:w-[80%] w-full">
-              <div className="font-bold text-[2.25rem]">2. Design</div>
+            <div className="lg:w-[80%] flex flex-col lg:gap-[2.9375rem] gap-[0.7rem] w-full">
+              <div className="font-bold lg:text-[2.25rem] text-[1rem] lg:text-start text-center">
+                2. Design
+              </div>
               <div className="lg:text-[1.45rem] text-[1rem] text-justify">
                 Design is key to capturing customer attention. We specialize in
                 fresh, creative designs and have been helping clients with Brand
@@ -63,11 +67,13 @@ const ProjectStatsSection = (): React.ReactElement => {
             </div>
           </div>
           <div className="flex lg:flex-row md:flex-row flex-col w-full gap-[2rem] items-center justify-center">
-            <div className="lg:w-[20%] w-[10rem]">
+            <div className="lg:w-[20%] w-[8rem]">
               <img className="size-full" src={BuildIcon} alt="Build Icon" />
             </div>
-            <div className="lg:w-[80%] w-full">
-              <div className="font-bold text-[2.25rem]">3. Build</div>
+            <div className="lg:w-[80%] flex flex-col lg:gap-[2.9375rem] gap-[0.7rem] w-full">
+              <div className="font-bold lg:text-[2.25rem] text-[1rem] lg:text-start text-center">
+                3. Build
+              </div>
               <div className="lg:text-[1.45rem] text-[1rem] text-justify">
                 Breath new life into your body brand by introducing it to the
                 digital world. We turn the impossible into reality for our
@@ -77,11 +83,13 @@ const ProjectStatsSection = (): React.ReactElement => {
             </div>
           </div>
           <div className="flex lg:flex-row-reverse md:flex-row-reverse flex-col w-full gap-[2rem] items-center justify-center">
-            <div className="lg:w-[20%] w-[10rem]">
+            <div className="lg:w-[20%] w-[8rem]">
               <img className="size-full" src={LaunchIcon} alt="Launch Icon" />
             </div>
-            <div className="lg:w-[80%] w-full">
-              <div className="font-bold text-[2.25rem]">4. Launch</div>
+            <div className="lg:w-[80%] flex flex-col lg:gap-[2.9375rem] gap-[0.7rem] w-full">
+              <div className="font-bold lg:text-[2.25rem] text-[1rem] lg:text-start text-center">
+                4. Launch
+              </div>
               <div className="lg:text-[1.45rem] text-[1rem] text-justify">
                 Breath new life into your brand by introducing it to the digital
                 world. We turn the impossible into reality for our customers.

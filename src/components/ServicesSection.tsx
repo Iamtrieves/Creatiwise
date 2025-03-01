9fad6c4ebd2354d9137ce1fdd394a38bc0eec402
Gabriel Indok David <indokdavid2020@gmail.com>
2025-03-01 13:49:33 +0100

    Update

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -3,7 +3,7 @@ import HomeCards from "./HomeCards";
 const ServicesSection = () => {
   return (
     <div className="mt-[4.5rem] lg:px-[4rem] px-[1.2rem]">
-      <div className="flex flex-col gap-3 mb-[4rem] justify-center items-center lg:text-[1.875rem]">
+      <div className="flex flex-col gap-3 lg:mb-[4rem] mb-[2.5rem] justify-center items-center lg:text-[1.875rem]">
         <div className="lg:text-[1.875rem] text-[1.4rem] font-medium">
           Our Services
         </div>
